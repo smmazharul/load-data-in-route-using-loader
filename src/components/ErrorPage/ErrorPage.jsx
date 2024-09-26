7211d0817ac1b16d764953de977dbdb94db367aa
smmazharul <hmoxygen@gmail.com>
2024-09-26 23:08:32 +0600

    use errorElement in router for notfound page

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Oops!!!</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,12 @@ import About from './components/About/About.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import Users from './components/Users/Users.jsx';
 import UserDetails from './components/UserDetails/UserDetails.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: "about",
