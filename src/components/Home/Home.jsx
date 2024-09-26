7db81b1664cce036da822bf84ea8d74b009425d8
smmazharul <hmoxygen@gmail.com>
2024-09-26 23:42:07 +0600

    use loading spinner from react-loader-spinner from awesome react

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
+import { Circles } from "react-loader-spinner";
 
 
 
 const Home = () => {
+    const navigation=useNavigation()
     return (
-        <div>
-            <Header></Header>
-            <h1>this is home page</h1>
-            <Outlet></Outlet>
-        
-        </div>
+      <div>
+        <Header></Header>
+        <h1>this is home page</h1>
+        {navigation.state === "loading" ? (
+          <Circles
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="circles-loading"
+            wrapperStyle={{ textAlign: "center" }}
+            wrapperClass=""
+            visible={true}
+          />
+        ) : (
+          <Outlet></Outlet>
+        )}
+      </div>
     );
 };
 
